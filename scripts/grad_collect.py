175c0da8e9312b83bdeeadf1ae4bdce1c4c24f6e
Krishnan <krshna@stanford.edu>
2023-04-11 23:24:26 -0400

    updated grad_collect to use env for env params, fix ag_return_body default

--- scripts/grad_collect.py
+++ scripts/grad_collect.py
@@ -4,6 +4,8 @@ from omegaconf import OmegaConf, DictConfig
 from shac import envs
 import numpy as np
 import torch
+from tqdm import tqdm
+from torchviz import make_dot
 
 
 @hydra.main(version_base="1.2", config_path="cfg", config_name="config.yaml")
@@ -16,19 +18,21 @@ def main(config: DictConfig):
 
     # create a random set of actions
     std = 0.5
+    n = env.num_obs
+    m = env.num_acts
     w = torch.normal(0.0, std, (env.num_envs, env.num_acts)).to(device)
     w[0] = w[0].zero_()
     fobgs = []
     zobgs = []
+    losses = []
+    baseline = []
 
     for h in tqdm(range(1, env.episode_length)):
-        print("h={:}".format(h))
         env.clear_grad()
         env.reset()
 
         ww = w.clone()
         ww.requires_grad_(True)
-
         loss = torch.zeros(env.num_envs).to(device)
 
         # apply first noisy action
@@ -41,11 +45,13 @@ def main(config: DictConfig):
             loss += rew
             # NOTE: commented out code below is for the debugging of more efficient grad computation
             # make_dot(loss.sum(), show_attrs=True, show_saved=True).render("correct_graph")
-            # loss.sum().backward(retain_graph=True)
+            # loss.sum().backward()
             # print(ww.grad)
             # exit(1)
 
         loss.sum().backward()
+        losses.append(loss.detach().cpu().numpy())
+        baseline.append(loss[0].detach().cpu().numpy())
 
         fobgs.append(ww.grad.cpu().numpy())
 
@@ -53,10 +59,19 @@ def main(config: DictConfig):
         zobg = 1 / std**2 * (loss.unsqueeze(1) - loss[0]) * ww
         zobgs.append(zobg.detach().cpu().numpy())
 
+    filename = "{:}_grads_{:}".format(env.__class__.__name__, env.episode_length)
+    if hasattr(env, "start_state"):
+        filename += "_" + str(env.start_state)
+    print("Saving to", filename)
     np.savez(
-        "{:}_grads_{:}".format(env.__class__.__name__, env.episode_length),
-        zobgs=np.array(zobgs),
-        fobgs=np.array(fobgs),
+        filename,
+        zobgs=zobgs,
+        fobgs=fobgs,
+        losses=losses,
+        baseline=baseline,
+        std=std,
+        n=n,
+        m=m,
     )
 
 
