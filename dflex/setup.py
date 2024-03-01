fdef70b07733a43702c2158b2854eec2caf8cf5b
Ignat Georgiev <ignat.m.georgiev@gmail.com>
2024-03-01 16:23:00 -0500

    take 5

--- dflex/setup.py
+++ dflex/setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     long_description="",
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    package_data={"": ["*.h", "*.stl", "*.xml", "*.urdf", "*.stl", "*.ojb", "*.usd", "*.dae", "*.jpg"]},
+    package_data={"dflex": ["*.h", "*.stl", "*.xml", "*.urdf", "*.stl", "*.ojb", "*.usd", "*.dae", "*.jpg"]},
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",  
